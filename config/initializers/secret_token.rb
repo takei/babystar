a96eea707fcf90450248ac6cc99a26d8db0a614f
takewo <st0098@gmail.com>
2012-10-17 19:54:57 +0900

    create an empty rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babystar::Application.config.secret_token = '1c43e11adbbeb81421932aba5ff7138ae5c54d4818e6ceba529bd95baeb91d6e1ef788417e97f6babd76524ff2d74f3c11e613ec08ad61d62076af38a020da3b'
