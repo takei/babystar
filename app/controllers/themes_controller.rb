f6284d4a1b1e3de520623576a66a24617edbeae4
takewo <st0098@gmail.com>
2012-11-14 18:03:44 +0900

    基本的なテーマのCRUDは作成した。というかほぼscaffoldの焼き直し

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -2,12 +2,37 @@ class ThemesController < ApplicationController
   def new
     @theme = Theme.new
   end
-  def list
+
+  def index
     @themes = Theme.all
   end
+
   def create
     @theme = Theme.new(params[:theme])
     @theme.users = params[:users].map{|user|User.find(user)}
-    @theme.save
+
+    respond_to do |format|
+      if @theme.save
+        format.html { redirect_to @theme, notice: 'Theme was successfully created.'}
+      else
+        format.html {render action: "new"}
+      end
+    end
+  end
+
+  def show
+    @theme = Theme.find(params[:id])
+  end
+
+  def edit
+    @theme = Theme.find(params[:id])
+  end
+
+  def destroy
+    @theme = Theme.find(params[:id])
+    @theme.destroy
+    respond_to do |format|
+      format.html {redirect_to themes_url}
+    end
   end
 end
