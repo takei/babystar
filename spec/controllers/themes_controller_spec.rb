c7fed0c6b3c0e740036cc59aa9352fdebdf80f0e
takewo <st0098@gmail.com>
2012-11-21 18:04:05 +0900

    スペック書いた

--- spec/controllers/themes_controller_spec.rb
+++ spec/controllers/themes_controller_spec.rb
@@ -1,19 +1,118 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe ThemesController do
+  fixtures :themes, :users
 
-  describe "GET 'new'" do
-    it "returns http success" do
+  describe "GET '/themes' (index)" do
+
+    before do
+      get 'index'
+    end
+
+    it "リクエストは成功すること" do
+      response.should be_success
+    end
+
+    it "テーマ一覧が取得できること" do
+      get 'index'
+      assigns[:themes].should have_at_least(1).themes
+    end
+
+    it "themes/index を描画すること" do
+      response.should render_template 'themes/index'
+    end
+  end
+
+  describe "GET 'themes/:id' (show)" do
+    before do
+      @theme = themes(:theme1)
+      get 'show', :id => @theme.id
+    end
+
+    it "レスポンスが成功すること" do
+      response.should be_success
+    end
+
+    it "指定したIDのテーマが取得されていること" do
+      assigns[:theme].should == @theme
+    end
+
+    it "themes/show を描画すること" do
+      response.should render_template "themes/show"
+    end
+  end
+
+  describe "GET 'themes/new (new)'" do
+
+    before do
       get 'new'
+    end
+
+    it "レスポンスが成功すること" do
+      response.should be_success
+    end
+
+    it "空のThemeがロードされること" do
+      assigns[:theme] == Theme.new
+    end
+
+    it "themes/new を描画すること" do
+      response.should render_template "themes/new"
+    end
+
+  end
+
+  describe "GET 'themes/:id/edit" do
+    before do
+      @theme = themes(:theme1)
+      get 'edit', :id => @theme.id
+    end
+
+    it "レスポンスが成功すること" do
+      response.should be_success
+    end
+
+    it "指定したIDのテーマが取得されていること" do
+      assigns[:theme].should == @theme
+    end
+
+    it "themes/edit が描画されること" do
+      response.should render_template "themes/edit"
+    end
+  end
+
+  describe "POST 'themes'" do
+    before do
+    end
+
+    it "レスポンスが成功すること" do
+      response.should be_success
+    end
+
+    it "" do
+    end
+
+
+  end
+
+  describe "PUT 'themes/:id'" do
+    before do
+
+    end
+
+    it "レスポンスが成功すること" do
       response.should be_success
     end
   end
 
-  describe "GET 'list'" do
-    it "returns http success" do
-      get 'list'
+  describe "DELETE 'themes/:id'" do
+    before do
+
+    end
+
+    it "レスポンスが成功すること" do
       response.should be_success
     end
   end
-
 end
