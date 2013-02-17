c45d4836d6a211289717a2384a44933e5b1ccd0b
omusubiman <omusubiman2010@gmail.com>
2013-02-17 13:24:43 +0900

    新topでの不要なテストを削除

--- spec/controllers/top_controller_spec.rb
+++ spec/controllers/top_controller_spec.rb
@@ -1,28 +1,10 @@
 require 'spec_helper'
 
 describe TopController do
-	fixtures :themes, :users, :contributors
 
-	context "when getting zero theme on GET indexBt" do
-		before do
-			Theme.stub!(:all).and_return([])
-		end
-		it 'renders themes/new_bt' do
-			get :indexBt
-			expect(response).to redirect_to newBt_themes_path
-		end
-	end
-
-	context "when getting some themes on GET indexBt" do
-		it 'renders top/index_bt' do
-			get :indexBt
-			expect(response).to render_template 'top/index_bt'
-		end
-	end
-
-	it 'show the specified theme and mentions on it' do
-		get :showBt, :theme_id => 1
-		expect(response).to render_template 'top/index_bt'
+	it 'renders top/index' do
+		get :index
+		expect(response).to render_template 'top/index'
 	end
 
 end
